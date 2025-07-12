a399719f686bf328a5357fae2d9c361c90a2b23c
Pradeep <wagh.pradeep2923@gmail.com>
2025-07-12 14:47:29 +0530

    Empty or base project

---
+++ EclipseTestProNew/src/test/java/BrowserStart/Browser_Init.java
@@ -0,0 +1,12 @@
+package BrowserStart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class Browser_Init 
+{
+	
+
+}
+	
