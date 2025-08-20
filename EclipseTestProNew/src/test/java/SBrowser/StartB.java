1ffc5d5529dc9b8182257999b0f0971a0c262dac
Pradeep <wagh.pradeep2923@gmail.com>
2025-08-20 22:49:23 +0530

    Add File Code

--- EclipseTestProNew/src/test/java/SBrowser/StartB.java
+++ EclipseTestProNew/src/test/java/SBrowser/StartB.java
@@ -3,5 +3,14 @@ package SBrowser;
 public class StartB 
 {
 	
+public static void main(String args[])
+{
+  add();   
+}
+
+public static void add()
+{
+   System.out.println("It is add method");
+}
 
 }
